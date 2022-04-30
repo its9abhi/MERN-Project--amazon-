c140fe7d904d873b6ee984d03b87de08cac22b2c
abhishek <alokyadav383@gmail.com>
2022-04-30 17:05:22 +0530

    commit on 'master'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazona</a>
-      </header>
-      <main>list Product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
